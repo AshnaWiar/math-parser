a8ff97de50784475acd89d2ba7229cfd6ff44441
AshnaWiar <ashna_wiar@hotmail.com>
2018-07-08 14:38:00 +0200

    upgraded to version 1.2!

--- src/main/java/com/wiar/mathparser/MathParser.java
+++ src/main/java/com/wiar/mathparser/MathParser.java
@@ -3,7 +3,7 @@ package main.java.com.wiar.mathparser;
 import main.java.com.wiar.mathparser.algorithm.Algorithm;
 
 public class MathParser {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     private final Algorithm algorithm;
 
     public MathParser() {
